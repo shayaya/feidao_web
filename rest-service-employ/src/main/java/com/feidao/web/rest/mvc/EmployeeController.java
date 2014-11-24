b40c7ed1dc59cffbe597cbb39eaf1cbb6e9050bb
zhaoyuheng <zhaoyuheng@dbs.com>
2014-11-25 00:48:35 +0800

    2014.11.25 - support jsp view in EmployeeController

--- rest-service-employ/src/main/java/com/feidao/web/rest/mvc/EmployeeController.java
+++ rest-service-employ/src/main/java/com/feidao/web/rest/mvc/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,6 +23,11 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
 	
+	/**
+	 * regular expression used to allow the dot(.) in email parameter
+	 * @param email
+	 * @return
+	 */
 	@RequestMapping(value ="/{email:.+}", method = RequestMethod.GET, produces = {"application/json; charset=utf-8", "application/xml; charset=utf-8"})
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
@@ -30,6 +36,20 @@ public class EmployeeController {
 		return employeeService.getEmployeeByEmail(email);
 	}
 	
+	@RequestMapping(value = "/jsp/{email:.+}", method = RequestMethod.GET)
+	public String getEmployeeByEmailJsp(@PathVariable(value="email") String email, Model model) {
+		Employee employee = employeeService.getEmployeeByEmail(email);
+		model.addAttribute("firstName", employee.getFirstName() );
+		
+		return "employSearchResult";
+	}
+	
+	
+	/**
+	 * To return list of object in xml format, the only thing which is working is to create some entities with JAXB annotations in order to serialize into XML.
+	 * @param firstName
+	 * @return
+	 */
 	@RequestMapping(value ="/name/{firstName}", method = RequestMethod.GET, produces = {"application/json; charset=utf-8", "application/xml; charset=utf-8"})
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
---
+++ rest-service-employ/src/main/webapp/WEB-INF/views/employSearchResult.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+<html>
+<head>
+	<title>Search Result</title>
+</head>
+<body>
+<h1>
+	Employee is found!  
+</h1>
+
+<P>  First Name is ${firstName}. </P>
+</body>
+</html>
